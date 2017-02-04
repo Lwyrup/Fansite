3e8d692feebae4a629b6d3dab15ddd94239273b1
Spencer Gerdes <skgerdes@yahoo.com>
2017-02-04 17:28:31 -0600

    moved from html to php

---
+++ external-links.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+	<title>Links - The Serj Tankian fan page</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <?php require("navagation.php") ?>
+	
+	<h2>Links</h2>
+
+	<div class="link-right">
+		<a href="http://serjtankian.com/" target=_blank>
+		<h3>Serj Tankian offical website</h3>
+		</a>
+		<p><img src="images/serj-grab.png" alt="Screenshot of Serj's webpsite" width="400"><br>Serj's offical site.</p>
+	</div>
+
+	<div class="link-left">
+		<a href="https://en.wikipedia.org/wiki/Serj_Tankian" target=_blank>
+			<h3>Serj Tankian on Wikipedia</h3>
+		</a>
+		<p><img src="images/wiki-grab.png" alt="Screenshot of wikipedia for Serj Tankian" width="400"><br>Wikipedia page for Serj Tankian.</p>
+	</div>
+	
+	<div class="link-right">
+		<a href="http://www.last.fm/music/System+of+a+Down" target=_blank>
+			<h3>last.fm - System of a Down</h3>
+		</a>
+		<p><img src="images/soad-last-grab.png" alt="Link to last.fm/System-of-a-Down" width="400"><br>Listen to System of a Down.</p>
+	</div>
+	
+	<div class="link-left">
+		<a href="http://www.last.fm/music/Serj+Tankian" target=_blank>
+			<h3>last.fm - Serj Tankian</h3>
+		</a>
+		<p><img src="images/last-grab.png" alt="Link to last.fm/System-of-a-Down" width="400"><br>Listen to Serj's music.</p>
+	</div>
+	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+	<title>Home - The Serj Tankian fan page</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+	<h1>Serj Tankian</h1>
+	<img src="images/serjhome.jpg" alt="SerjTankian" id="serj-home" width=400>
+	<?php include("navagation.php")?>
+</body>
+</html>
